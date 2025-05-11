a817d0ef6428fc226f4ac3cefa4f93d7ef1fd81a
9armoude <130075341+9armoude@users.noreply.github.com>
2025-05-11 11:01:05 +0100

    Update rosetta_vnc_config.sh

--- rosetta_vnc_config.sh
+++ rosetta_vnc_config.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# Check if docker-compose.yaml exists and remove it if so
+# Check if docker-compose.yaml exists and remove it
 if [ -f docker-compose.yaml ]; then
   echo "Removing existing docker-compose.yaml..."
   rm docker-compose.yaml
 fi
 
-# Download the latest docker-compose.yaml from GitHub
+# Download the latest docker-compose.yaml
 echo "Downloading new docker-compose.yaml..."
 wget https://raw.githubusercontent.com/9armoude/rosetta_vnc/test/docker-compose.yaml
+
+# Remove unsupported version line
+sed -i '/^version:/d' docker-compose.yaml
 
 # Start the container
 docker-compose up -d
 
-# Wait a few seconds to ensure the container is fully started
+# Wait for the container to start
 sleep 3
 
-# Run Google Chrome inside the container
+# Run Google Chrome in the container
 docker exec -d rosetta_container google-chrome-stable --no-sandbox
