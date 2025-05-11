16901b701e80b5016f75dae092c6e26cbeb0bb0d
9armoude <130075341+9armoude@users.noreply.github.com>
2025-05-11 10:59:36 +0100

    Update rosetta_vnc_config.sh

--- rosetta_vnc_config.sh
+++ rosetta_vnc_config.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Create and run the container
+# Check if docker-compose.yaml exists and remove it if so
+if [ -f docker-compose.yaml ]; then
+  echo "Removing existing docker-compose.yaml..."
+  rm docker-compose.yaml
+fi
+
+# Download the latest docker-compose.yaml from GitHub
+echo "Downloading new docker-compose.yaml..."
+wget https://raw.githubusercontent.com/9armoude/rosetta_vnc/test/docker-compose.yaml
+
+# Start the container
 docker-compose up -d
 
 # Wait a few seconds to ensure the container is fully started
