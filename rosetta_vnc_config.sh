0c8e13480e81d21f9505f7f7389be67b1260b9a3
9armoude <130075341+9armoude@users.noreply.github.com>
2025-05-11 10:49:03 +0100

    Update rosetta_vnc_config.sh

--- rosetta_vnc_config.sh
+++ rosetta_vnc_config.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # Create and run the container
-docker run -d \
-  --name rosetta_container \
-  --user 0 \
-  --shm-size=256m \
-  -p 5901:5901 \
-  -p 6901:6901 \
-  -v headless:/headless \
-  vinexborsalino/rosetta_vnc
+docker-compose up -d
 
 # Wait a few seconds to ensure the container is fully started
 sleep 3
